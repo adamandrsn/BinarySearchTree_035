f63625b015c38948e74295d6f15fcd52e754b7f2
Adam Anderson <adam.anderson.ft21@mail.umy.ac.id>
2022-12-28 17:48:25 +0700

    add static void Main

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -81,7 +81,7 @@ namespace BinarySearchTree
         if (ptr != null)
         {
             inorder(ptr.leftchild);
-            Console.WriteLine(ptr.info + "");
+            Console.Write(ptr.info + "");
             inorder(ptr.rightchild);
         }
     }
@@ -112,6 +112,44 @@ namespace BinarySearchTree
             postorder(ptr.leftchild);
             postorder(ptr.rightchild);
             Console.WriteLine(ptr.info + "");
+        }
+    }
+    static void Main(string[] args)
+    {
+        Program x = new Program();
+        while (true)
+        {
+            Console.WriteLine("\nMenu");
+            Console.WriteLine("1. Implemental insert option");
+            Console.WriteLine("2. Perform inorder traversal");
+            Console.WriteLine("3. Perform preorder traversal");
+            Console.WriteLine("4. Perform postorder traversal");
+            Console.WriteLine("5. Exit");
+            Console.WriteLine("\nEnter your choice (1-5):     ");
+            char ch = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine();
+            switch (ch)
+            {
+                case '1':
+                    {
+                        Console.Write("Enter a word:  ");
+                        string word = Console.ReadLine();
+                        x.insert(word);
+                    }
+                    break;
+                case '2':
+                    {
+                        x.inorder(x.ROOT);
+                    }
+                    break;
+                case '3':
+                    {
+                        x.preorder(x.ROOT);
+                    }
+                    break;
+                case '4':
+                    { }
+            }
         }
     }
 }
